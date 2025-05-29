2d91b336259a459588e2a82a7efde19f25bd20c1
JurgenDelira <arq.jorgedelira@gmail.com>
2025-05-29 11:06:20 -0700

    29

---
+++ src/OperadorTarea.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class OperadorTarea {
+    public static void main(String[] args) {
+
+        Integer n1 = 0;
+        Integer n2 = 0;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número: ");
+        n1 = s.nextInt();
+
+        System.out.println("Ingrese el segundo número: ");
+        n2 = s.nextInt();
+
+
+        if (n1 > n2){
+            System.out.println(n1 + ", " + n2);
+        } else if (n2 > n1){
+            System.out.println(n2+ ", " + n1);
+        } else {
+            System.out.println("Números son iguales");
+        }
+
+    }
+}

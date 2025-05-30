20b97c39117bc43d9572ea73ea368b8489f32f0b
JurgenDelira <arq.jorgedelira@gmail.com>
2025-05-30 10:18:14 -0700

    34

--- src/FlujosDeControl/SentenciaSwitchCase.java
+++ src/FlujosDeControl/SentenciaSwitchCase.java
@@ -1,7 +1,57 @@
 package FlujosDeControl;
 
+import java.util.Scanner;
+
 public class SentenciaSwitchCase {
     public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingrese el número de mes entre 1 y 12:");
+        int mes = s.nextInt();
+        String nombreMes = null;
+
+        switch (mes){
+            case 1:
+                nombreMes = "Enero";
+                break;
+            case 2:
+                nombreMes = "Febrero";
+                break;
+            case 3:
+                nombreMes = "Marzo";
+                break;
+            case 4:
+                nombreMes = "Abril";
+                break;
+            case 5:
+                nombreMes = "Mayo";
+                break;
+            case 6:
+                nombreMes = "Junio";
+                break;
+            case 7:
+                nombreMes = "Julio";
+                break;
+            case 8:
+                nombreMes = "Agosto";
+                break;
+            case 9:
+                nombreMes = "Septiembre";
+                break;
+            case 10:
+                nombreMes = "Octubre";
+                break;
+            case 11:
+                nombreMes = "Noviembre";
+                break;
+            case 12:
+                nombreMes = "Diciembre";
+                break;
+            default:
+                nombreMes = "Indefinido";
+                break;
+        }
+        System.out.println("nombreMes = " + nombreMes);
 
         char num = 'z';
         switch (num){

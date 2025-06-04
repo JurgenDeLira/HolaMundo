89809f52d5c7bcb0932a61107bdab72200d30cbd
JurgenDelira <arq.jorgedelira@gmail.com>
2025-06-04 11:12:47 -0700

    53

--- src/DateYCalendar/EjemploJavaUtilDate.java
+++ src/DateYCalendar/EjemploJavaUtilDate.java
@@ -13,6 +13,15 @@ public class EjemploJavaUtilDate {
         SimpleDateFormat df = new SimpleDateFormat("EEEE dd 'de' MMMM, yyyy");
         String fechaStr = df.format(fecha);
 
+        long j = 0;
+        for(int i = 0; i < 100000000; i++){
+            j += i;
+        }
+        System.out.println("j = " + j);
+
+        Date fecha2 = new Date();
+        long tiempoFinal = fecha2.getTime() - fecha.getTime();
+        System.out.println("Tiempo transcurrido en el for = " + tiempoFinal);
         System.out.println("fechaStr = " + fechaStr);
     }
 }

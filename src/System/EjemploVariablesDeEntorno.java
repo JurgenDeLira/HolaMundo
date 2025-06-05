0521957252ec49dac9cdc0cb3546b7e52b57988c
JurgenDelira <arq.jorgedelira@gmail.com>
2025-06-05 11:44:08 -0700

    60

---
+++ src/System/EjemploVariablesDeEntorno.java
@@ -0,0 +1,39 @@
+package System;
+
+import java.util.Map;
+
+public class EjemploVariablesDeEntorno {
+    public static void main(String[] args) {
+
+        Map<String, String> varEnv = System.getenv();
+        System.out.println("Variables de ambiente del sistema = " + varEnv);
+
+        System.out.println("------- Listando variables de entorno del sistema ---------");
+        for (String key: varEnv.keySet()){
+            System.out.println(key + " => " + varEnv.get(key));
+        }
+
+        String username = System.getenv("USERNAME");
+        System.out.println("username = " + username);
+
+        String javaHome = System.getenv("JAVA_HOME");
+        System.out.println("javaHome = " + javaHome);
+
+        String tempDir = System.getenv("TEMP");
+        System.out.println("tempDir = " + tempDir);
+
+        String path = System.getenv("PATH");
+        System.out.println("path = " + path);
+
+        String path2 = varEnv.get("PATH");
+        System.out.println("path2 = " + path2);
+
+        String appEnv = varEnv.get("APPLICATION_ENV");
+        System.out.println("appEnv = " + appEnv);
+
+        String hola = varEnv.get("SALUDAR_HOLA");
+        System.out.println("hola = " + hola);
+
+
+    }
+}

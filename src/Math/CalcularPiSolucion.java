c49a1fe089a9772a8f03c4cf8a21c5ff2b09af08
JurgenDelira <arq.jorgedelira@gmail.com>
2025-06-09 10:21:22 -0700

    66

---
+++ src/Math/CalcularPiSolucion.java
@@ -0,0 +1,18 @@
+package Math;
+
+import java.util.Scanner;
+
+public class CalcularPiSolucion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double area; // el área a calcular
+
+        double radio; // radio a ingresar
+        System.out.println("Introduce el radio de un circulo: ");
+        radio = scanner.nextDouble();
+        area = Math.PI * Math.pow(radio, 2);
+
+        System.out.println("El área de una circunferencia de radio " + radio + " es: " + area);
+    }
+}

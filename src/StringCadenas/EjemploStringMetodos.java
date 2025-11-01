9fcf09cc8e8e8fba140b02070f1871c2896d8e68
JurgenDelira <arq.jorgedelira@gmail.com>
2025-10-31 17:00:10 -0700

    cambio EjemploStringMetodos

--- src/StringCadenas/EjemploStringMetodos.java
+++ src/StringCadenas/EjemploStringMetodos.java
@@ -11,29 +11,35 @@ public class EjemploStringMetodos {
         System.out.println("nombre.equals(\"Jorge\") = " + nombre.equals("Jorge"));
         System.out.println("nombre.equals(\"jorge\") = " + nombre.equals("jorge"));
         System.out.println("nombre.equalsIgnoreCase(\"jorge\") = " + nombre.equalsIgnoreCase("jorge"));
-        System.out.println("nombre.compareTo(\"\") = " + nombre.compareTo("Jorge"));
+        System.out.println("nombre.compareTo(\"Jorge\") = " + nombre.compareTo("Jorge"));
         System.out.println("nombre.compareTo(\"Aaron\") = " + nombre.compareTo("Aaron"));
         System.out.println("nombre.charAt(0) = " + nombre.charAt(0));
         System.out.println("nombre.charAt(1) = " + nombre.charAt(1));
-        System.out.println("nombre.charAt(4) = " + nombre.charAt(4));
+        System.out.println("nombre.charAt(5) = " + nombre.charAt(4));
         System.out.println("nombre.charAt(nombre.length()-1) = " + nombre.charAt(nombre.length()-1));
 
-        System.out.println("nombre.substring(3) = " + nombre.substring(3));
-        System.out.println("nombre.substring(1, 3) = " + nombre.substring(1, 3));
+        System.out.println("\n");
+
+        System.out.println("nombre.substring(1) = " + nombre.substring(2));
+        System.out.println("nombre.substring(1, 4) = " + nombre.substring(1, 4));
         System.out.println("nombre.substring(3, 5) = " + nombre.substring(3, 5));
-        System.out.println("nombre.substring(nombre.length()-1) = " + nombre.substring(nombre.length()-1));
+        System.out.println("nombre.substring(nombre.length()-2) = " + nombre.substring(nombre.length()-2));
+
+        System.out.println("\n");
 
         String trabalenguas = "trabalenguas";
-        System.out.println("trabalenguas.replace() = " + trabalenguas.replace("a", "."));
+        System.out.println("trabalenguas = " + trabalenguas.replace("a", "."));
         System.out.println("trabalenguas = " + trabalenguas);
         System.out.println("trabalenguas.indexOf('a') = " + trabalenguas.indexOf('a'));
         System.out.println("trabalenguas.lastIndexOf('a') = " + trabalenguas.lastIndexOf('a'));
-        System.out.println("trabalenguas.indexOf('z') = " + trabalenguas.indexOf('z'));
-        System.out.println("trabalenguas.contains(\"lenguas\") = " + trabalenguas.contains("lenguas"));
+        System.out.println("trabalenguas.indexOf(\"z\") = " + trabalenguas.indexOf("lenguas"));
+        System.out.println("trabalenguas.contains(\"z\") = " + trabalenguas.contains("lenguas"));
         System.out.println("trabalenguas.startsWith(\"traba\") = " + trabalenguas.startsWith("traba"));
         System.out.println("trabalenguas.endsWith(\"lenguas\") = " + trabalenguas.endsWith("lenguas"));
-        System.out.println(" trabalenguas ");
-        System.out.println(" trabalenguas ".trim());
-    }
 
+        System.out.println("\n");
+
+        System.out.println("  trabalenguas ");
+        System.out.println("trabalenguas".trim());
+    }
 }
